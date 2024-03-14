c31bf1fdfc9c971cbbd0fa76601fe7d2fb955ab7
SabinoOrbello <gemellus95@outlook.it>
2024-03-14 17:49:54 +0100

    giorno 4,7

--- PizzoriaRistorante/Controllers/HomeController.cs
+++ PizzoriaRistorante/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PizzoriaRistorante.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace PizzoriaRistorante.Controllers
 {
     public class HomeController : Controller
     {
+        private ModelDbContext db = new ModelDbContext();
 
         public ActionResult Index()
         {
@@ -21,6 +23,18 @@ namespace PizzoriaRistorante.Controllers
             ViewBag.Title = "Back Office";
 
             return View();
+        }
+
+        public ActionResult TotaleOrdiniEvasi()
+        {
+            // Ottieni tutti gli ordini con lo stato "Finalizzato"
+            var ordiniFinalizzati = db.Ordini.Where(o => o.Status == "evaso");
+
+            // Calcola il totale degli ordini finalizzati
+            int totale = ordiniFinalizzati.Count();
+
+            // Restituisci il totale
+            return View(totale); // Assicurati di avere una vista che può visualizzare un singolo numero intero
         }
     }
 }
